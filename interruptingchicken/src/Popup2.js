0535748e7aef663f641c5a56a1336c83cb8ccca6
jacksonn <jacksonn@sfu.a>
2021-02-13 17:45:11 -0800

    added keyWords to popup

--- interruptingchicken/src/Popup2.js
+++ interruptingchicken/src/Popup2.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Popup.css';
+import useKeyPress from './hooks/useKeyPress';
+
+var keyWords = [
+    "cluck",
+    "bok",
+    "egg",
+    "chicken",
+    "hen"
+];
 
 function Popup(props) {
+    useKeyPress(key => {
+
+    });
+
+    var index;
+    index = Math.floor(Math.random()*keyWords.length);
+    console.log(index);
+
     return (
       <div className='popup'>
         <div className='popup_inner'>
-          <h1>{props.text}</h1>
+          <h1>{keyWords[index]}</h1>
           <button onClick={props.closePopup}>close me</button>
         </div>
       </div>
