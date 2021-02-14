87621a2b7302d4716c0202744e18a2a2f23fe932
johnmarquez12 <ajxz12@gmail.com>
2021-02-13 20:59:43 -0800

    added random chicken words

--- interruptingchicken/src/Popup.js
+++ interruptingchicken/src/Popup.js
@@ -3,12 +3,21 @@ import './Popup.css';
 import InputForm from './Components/InputForm'
 
 class Popup extends React.Component {
+
+  keyWords = [
+    "cluck",
+    "bok",
+    "egg",
+    "chicken",
+    "hen"
+  ];
+
   render() {
     return (
       <div className='popup'>
         <div className='popup_inner'>
           <h1>{this.props.text}</h1>
-          <InputForm text={this.props.text} closePopup={this.props.closePopup}></InputForm>
+          <InputForm text={keyWords[Math.floor(Math.random()*keyWords.length)]} closePopup={this.props.closePopup}></InputForm>
           {/* <button onClick={this.props.closePopup}>close me</button> */}
         </div>
       </div>
