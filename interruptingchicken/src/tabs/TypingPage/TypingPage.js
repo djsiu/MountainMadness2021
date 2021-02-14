a17a977c65e0a1dd3abdfc6aa190ac293f208556
jacksonn <jacksonn@sfu.a>
2021-02-13 19:39:52 -0800

    cleaned up TypingPage var names, added pictures

--- interruptingchicken/src/tabs/TypingPage/TypingPage.js
+++ interruptingchicken/src/tabs/TypingPage/TypingPage.js
@@ -1,9 +1,11 @@
-import logo from '../../logo.svg';
+//import logo from '../../logo.svg';
+import logo_chick from '../../chickens/white-chick.png'
 import './TypingPage.css';
 import {generate} from '../../utils/words'
 import useKeyPress from '../../hooks/useKeyPress'
 import React, {useState} from 'react';
 import Popup from '../../Popup2'
+import Timer from '../../Components/Timer';
 
 
 function TypingPage() {
@@ -17,10 +19,10 @@ function TypingPage() {
   const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
   const [incomingChars, setIncomingChars] = useState(initialWords.substr(1))
   const [currentCharCorrect, setCurrentCharCorrect] = useState(true);
-  const [randomVar, setRandomVar] = useState(false);
+  const [popupFlag, setPopupFlag] = useState(false);
 
-  function toggleVar() {
-    setRandomVar(!randomVar);
+  function toggleVars() {
+    setPopupFlag(!popupFlag);
   }
 
   useKeyPress(key => {
@@ -32,7 +34,7 @@ function TypingPage() {
     let updatedIncomingChars = incomingChars;
     
     //2
-    if (key === currentChar) {
+    if (key === currentChar && !popupFlag) {
       //3
       if (leftPadding.length > 0) {
         setLeftPadding(leftPadding.substring(1));
@@ -51,7 +53,7 @@ function TypingPage() {
       }
       setIncomingChars(updatedIncomingChars);
     } else {
-      setRandomVar(true);
+      setPopupFlag(true);
       setCurrentCharCorrect(false);
     }
   });
@@ -59,7 +61,7 @@ function TypingPage() {
   return (
     <div className="TypingPage">
       <header className="TypingPage-header">
-        <img src={logo} className="TypingPage-logo" alt="logo" />
+        <img src={logo_chick} className="TypingPage-logo" alt="logo" />
         <p className="Character">
           <span className="Character-out">
             {(leftPadding + outgoingChars).slice(-20)}
@@ -68,84 +70,9 @@ function TypingPage() {
           <span>{incomingChars.substr(0, 20)}</span>
         </p>
       </header>
-      {randomVar ? <Popup text='end me' closePopup={toggleVar}/> : null}
+      {popupFlag ? <Popup text='end me' closePopup={toggleVars}/> : null}
     </div>
   );
 }
 
 export default TypingPage;
-
-
-// class TypingPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     const initialWords = generate();
-
-//     this.state = {
-//       leftPadding: new Array(20).fill(' ').join(''),
-//       rightPadding: new Array(20).fill(' ').join(''),
-//       outgoingChars: '',
-//       setOutgoingChars: '',
-//       incomingChars: initialWords.charAt(0),
-//       setCurrentChar: initialWords.charAt(0),
-//       incomingChars: initialWords.substr(1),
-//       setIncomingChars: initialWords.substr(1)
-//     }
-//   } 
-
-//   handleTyping() {
-//     useKeyPress(key => {
-//       //1
-//       let updatedOutgoingChars = outgoingChars;
-//       let updatedIncomingChars = incomingChars;
-      
-//       //2
-//       if (key === currentChar) {
-//         //3
-//         if (leftPadding.length > 0) {
-//           setLeftPadding(leftPadding.substring(1));
-//         }
-//         //4
-//         updatedOutgoingChars += currentChar;
-//         setOutgoingChars(updatedOutgoingChars);
-        
-//         //5      
-//         setCurrentChar(incomingChars.charAt(0));
-        
-//         //6
-//         updatedIncomingChars = incomingChars.substring(1);
-//         if (updatedIncomingChars.split(' ').length < 10) {
-//           updatedIncomingChars +=' ' + generate();
-//         }
-//         setIncomingChars(updatedIncomingChars);
-//       }
-//     });
-//   }
-
-
-//   render () {
-//     return (
-//           <div className="TypingPage">
-//             <header className="TypingPage-header">
-//               <img src={logo} className="TypingPage-logo" alt="logo" />
-//               <p className="Character">
-//                 <span className="Character-out">
-//                   {(leftPadding + outgoingChars).slice(-20)}
-//                 </span>
-//                 <span className="Character-current">{currentChar}</span>
-//                 <span>{incomingChars.substr(0, 20)}</span>
-//               </p>
-//               <a
-//                 className="TypingPage-link"
-//                 href="https://reactjs.org"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 Learn React
-//               </a>
-//             </header>
-//           </div>
-//         );
-//   }
-// }
