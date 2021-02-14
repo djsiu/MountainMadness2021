4cfb4b2a0aa3a2cd3c01f94fc8a16acc77448f2b
johnmarquez12 <ajxz12@gmail.com>
2021-02-13 18:06:00 -0800

    fixed everything to tabs

--- interruptingchicken/src/App.js
+++ interruptingchicken/src/App.js
@@ -1,84 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {generate} from './utils/words'
-import useKeyPress from './hooks/useKeyPress'
-import React, {useState} from 'react';
-import PopBtn from './PopBtn';
-import Popup from './Popup2';
 
+import React from 'react';
+
+import TypingPage from './tabs/TypingPage/TypingPage'
 
 function App() {
-
-  const initialWords = generate()
-
-  const [leftPadding, setLeftPadding] = useState(
-    new Array(20).fill(' ').join(''),
-  );
-  const [outgoingChars, setOutgoingChars] = useState('');
-  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
-  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1))
-  const [currentCharCorrect, setCurrentCharCorrect] = useState(true);
-  const [randomVar, setRandomVar] = useState(false);
-
-  function toggleVar() {
-    setRandomVar(!randomVar);
-  }
-
-  useKeyPress(key => {
-
-    //1
-    setCurrentCharCorrect(true);
-
-    let updatedOutgoingChars = outgoingChars;
-    let updatedIncomingChars = incomingChars;
-    
-    //2
-    if (key === currentChar) {
-      //3
-      if (leftPadding.length > 0) {
-        setLeftPadding(leftPadding.substring(1));
-      }
-      //4
-      updatedOutgoingChars += currentChar;
-      setOutgoingChars(updatedOutgoingChars);
-      
-      //5      
-      setCurrentChar(incomingChars.charAt(0));
-      
-      //6
-      updatedIncomingChars = incomingChars.substring(1);
-      if (updatedIncomingChars.split(' ').length < 10) {
-        updatedIncomingChars +=' ' + generate();
-      }
-      setIncomingChars(updatedIncomingChars);
-    } else {
-      setRandomVar(true);
-      setCurrentCharCorrect(false);
-    }
-  });
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Character">
-          <span className="Character-out">
-            {(leftPadding + outgoingChars).slice(-20)}
-          </span>
-          {currentCharCorrect ? <span className="Character-current">{currentChar}</span> : <span className="Character-current-incorrect">{currentChar}</span>}
-          <span>{incomingChars.substr(0, 20)}</span>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {randomVar ? <Popup text='end me' closePopup={toggleVar}/> : null}
-    </div>
+    <TypingPage/>
   );
 }
 
