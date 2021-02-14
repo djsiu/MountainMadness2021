93a4de4b3bf0ff78cd691585e500724014bc447e
johnmarquez12 <ajxz12@gmail.com>
2021-02-14 00:25:00 -0800

    added reset button

--- interruptingchicken/src/Components/BtnComponent.js
+++ interruptingchicken/src/Components/BtnComponent.js
@@ -3,24 +3,24 @@ import React from 'react';
 function BtnComponent(props) {
   return (
     <div>
-      {(props.status === 0)? 
+      {/* {(props.status === 0)? 
         <button className="stopwatch-btn stopwatch-btn-gre"
         onClick={props.start}>Start</button> : ""
-      }
+      } */}
 
-      {(props.status === 1)? 
+      {/* {(props.status === 1)? 
         <div>
           <button className="stopwatch-btn stopwatch-btn-red"
                   onClick={props.stop}>Stop</button>
           <button className="stopwatch-btn stopwatch-btn-yel"
                   onClick={props.reset}>Reset</button>
         </div> : ""
-      }
+      } */}
 
      {(props.status === 2)? 
         <div>
-          <button className="stopwatch-btn stopwatch-btn-gre"
-                  onClick={props.resume}>Resume</button>
+          {/* <button className="stopwatch-btn stopwatch-btn-gre"
+                  onClick={props.resume}>Resume</button> */}
           <button className="stopwatch-btn stopwatch-btn-yel"
                   onClick={props.reset}>Reset</button>
         </div> : ""
